4615a2ef32fded7d203176c3cec6759ccf15fe96
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-02-26 23:05:26 +0900

    D-2
    v-1.1#5
    ---
    1. TINKER
    
        ==> done

--- main.cpp
+++ main.cpp
@@ -53,6 +53,7 @@ dBodyID ball;      // an ball
 //const dReal radius = 3, mass = 1.0;// radius(m)、weight(kg) of an ball
 dReal radius = 0.2;// radius(m)、weight(kg) of an ball
 const dReal mass = 1.0;// radius(m)、weight(kg) of an ball
+//const dReal mass = 3.0;// radius(m)、weight(kg) of an ball
 
 ///////////////////////////////
 //
@@ -64,11 +65,29 @@ float color[3] = {1.0, 0, 1.0};
 
 float count = 0;
 
+float head = 90.0;
+float pitch = -15.0;
+float roll = 0.0;
+
+float pos_X = 0.0;
+float pos_Y = -7.0;
+//float pos_Y = -3.0;
+float pos_Z = 3.0;
+
+float grav_Z = -0.001;
+
+float step = 0.05;
+
+dReal pos0_Z = 4.0;
+
+
 // Simulation loop
 static void simLoop (int pause)
 {
 	const dReal *pos,*R;//position, rotation matrix
-	dWorldStep(world,0.05);// Step a simulation world, time step is 0.05 [s]
+	dWorldStep(world, step);// Step a simulation world, time step is 0.05 [s]
+//	dWorldStep(world,0.01);// Step a simulation world, time step is 0.05 [s]
+//	dWorldStep(world,0.05);// Step a simulation world, time step is 0.05 [s]
 
 //	dsSetColor(1.0,0.0,0.0);  // Set color  (red, green, blue) value is from 0 to 1.0
 //	dsSetColor(color[0], color[1], color[2]);  // Set color  (red, green, blue) value is from 0 to 1.0
@@ -89,15 +108,22 @@ static void simLoop (int pause)
 void start()
 {
 	// Set a camera
-	static float xyz[3] = {0.0,-3.0,1.0};   // View position (x, y, z [m])
-	static float hpr[3] = {90.0,0.0,0.0};  // View direction （head, pitch, roll[°]）
+	static float xyz[3] = {pos_X, pos_Y, pos_Z};   // View position (x, y, z [m])
+//	static float xyz[3] = {0.0,-3.0, pos_Z};   // View position (x, y, z [m])
+
+//	static float xyz[3] = {0.0,-3.0,1.0};   // View position (x, y, z [m])
+	static float hpr[3] = {head, pitch, roll};  // View direction （head, pitch, roll[°]）
+//	static float hpr[3] = {head,0.0,0.0};  // View direction （head, pitch, roll[°]）
+//	static float hpr[3] = {90.0,0.0,0.0};  // View direction （head, pitch, roll[°]）
 	dsSetViewpoint (xyz,hpr);// Set a view point
 }
 
 // main function
 int main (int argc, char **argv)
 {
-	dReal x0 = 0.0, y0 = 0.0, z0 = 1.0; // initial position of an ball[m]
+	dReal x0 = 0.0, y0 = 0.0, z0 = pos0_Z; // initial position of an ball[m]
+//	dReal x0 = 0.0, y0 = 0.0, z0 = 3.0; // initial position of an ball[m]
+//	dReal x0 = 0.0, y0 = 0.0, z0 = 1.0; // initial position of an ball[m]
 //  dReal x0 = 0.0, y0 = 0.0, dReal z0 = 1.0; // initial position of an ball[m]
 	dMass m1; // mass parameter
 
@@ -112,7 +138,12 @@ int main (int argc, char **argv)
 
 	dInitODE();      // Initialize ODE
 	world = dWorldCreate();  // Create a dynamic world
-	dWorldSetGravity(world,0,0,-0.001);// Set gravity（x, y, z)
+//	dWorldSetGravity(world,0.0001,0.0001,-0.0001);// Set gravity（x, y, z)
+//	dWorldSetGravity(world,0,0,0.0001);// Set gravity（x, y, z)
+//	dWorldSetGravity(world,0,0,-0.0001);// Set gravity（x, y, z)
+//	dWorldSetGravity(world,0,0,-0.003);// Set gravity（x, y, z)
+//	dWorldSetGravity(world,0,0,-0.001);// Set gravity（x, y, z)
+	dWorldSetGravity(world,0,0, grav_Z);// Set gravity（x, y, z)
 
 	//Create a ball
 	ball = dBodyCreate(world); //  Crate a rigid body
@@ -128,5 +159,9 @@ int main (int argc, char **argv)
 
 	dWorldDestroy(world);// Destroy the world
 	dCloseODE();                 // Close ODE
+
+	printf("done\n");
+
+
 	return 0;
 }
